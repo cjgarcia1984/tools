767428ba3df080a9e3a8d8255499b8e4dd9524b4
Chris <cjgarcia1984@gmail.com>
2023-06-05 00:58:49 -0700

    UPDATED: Added function for loading config file and getting timestamp.
    - Added load_config function.
    - Added get_timestamp function.

--- general.py
+++ general.py
@@ -1,10 +1,19 @@
 import pandas as pd
 from dateutil import parser
+import yaml
+from datetime import datetime
+
+
+def load_config(config_file):
+    with open(config_file, "r") as file:
+        return yaml.safe_load(file)
 
 
 def standardize_date(s, time="D"):
     s = pd.to_datetime(s.apply(parser.parse), utc=True)
-    s = (
-        s.dt.tz_localize(None).dt.floor(time)
-    )
+    s = s.dt.tz_localize(None).dt.floor(time)
     return s
+
+
+def get_timestamp():
+    return datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
