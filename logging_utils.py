2247aa747d929c963919bc6e4309353c5c3ba5b0
Chris <cjgarcia1984@gmail.com>
2023-07-05 21:51:29 -0700

    updates

--- logging_utils.py
+++ logging_utils.py
@@ -1,13 +1,20 @@
 import logging
+import sys
 
+def setup_logging(file_path):
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
 
-def setup_logging():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        filename="app.log",
-        filemode="a",
-    )
+    handler = logging.FileHandler(file_path, 'w')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
 
+    # clear all handlers
+    root.handlers = []
+    root.addHandler(handler)
+
+    # redirect stdout and stderr
+    sys.stdout = handler.stream
+    sys.stderr = handler.stream
 
 logger = logging.getLogger(__name__)
