7c9142fbdddefe7482571a205cfb7ca66d92c2bc
Chris <cjgarcia1984@gmail.com>
2023-07-07 11:25:56 -0700

    ADDED: Updated logging setup
    - Added the to specify a custom file path and name
    - Added debug mode to log to console instead of file when enabled
    - Updated logging format for timestamp and log level

--- logging_utils.py
+++ logging_utils.py
@@ -1,20 +1,24 @@
 import logging
 import sys
 
-def setup_logging(file_path):
-    root = logging.getLogger()
-    root.setLevel(logging.INFO)
+def setup_logging(path, name, debug=False):
+    if not debug:
+        root = logging.getLogger()
+        root.setLevel(logging.INFO)
+        file_path = f"{path}/{name}.log"
+        print(f"Debug mode off: logging to {file_path}")
+        handler = logging.FileHandler(file_path, 'w')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
 
-    handler = logging.FileHandler(file_path, 'w')
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+        # clear all handlers
+        root.handlers = []
+        root.addHandler(handler)
 
-    # clear all handlers
-    root.handlers = []
-    root.addHandler(handler)
-
-    # redirect stdout and stderr
-    sys.stdout = handler.stream
-    sys.stderr = handler.stream
+        # redirect stdout and stderr
+        sys.stdout = handler.stream
+        sys.stderr = handler.stream
+    else:
+        print("Debug mode on: logging to console")
 
 logger = logging.getLogger(__name__)
