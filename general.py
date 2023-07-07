0ab11c944fbefd35326b1446fa5fb01b48b981cb
Chris <cjgarcia1984@gmail.com>
2023-07-07 11:16:58 -0700

    UPDATED: Added is_same_dir Added os  Added is_same_dir function

--- general.py
+++ general.py
@@ -2,7 +2,7 @@ import pandas as pd
 from dateutil import parser
 import yaml
 from datetime import datetime
-
+import os
 
 def load_config(config_file):
     with open(config_file, "r") as file:
@@ -17,3 +17,10 @@ def standardize_date(s, time="D"):
 
 def get_timestamp():
     return datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+
+def is_same_dir(path,cwd=None):
+    if cwd is None:
+        cwd = os.path.dirname(os.path.realpath(__file__))
+    cwd = os.path.normpath(cwd)  # Normalize the path
+    repo_dir = os.path.normpath(path)  # replace with your repo's path
+    return cwd == repo_dir
